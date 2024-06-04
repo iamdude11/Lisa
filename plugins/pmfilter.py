e1155a73f5114f6d26f01df617e2e24e0f559225
Mr kuttusan <yedhukrishnanu2001@gmail.com>
2024-06-04 18:28:48 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2798,10 +2798,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "group_info":
         buttons = [[
-            InlineKeyboardButton('🍒 ᴀʟʟ ᴏᴜʀ ʟɪɴᴋꜱ ', url="https://t.me/PCMOffcL")
+            InlineKeyboardButton('🍒 ᴀʟʟ ᴏᴜʀ ʟɪɴᴋꜱ ', url="https://t.me/XeonFlix")
        ],[
             InlineKeyboardButton('📮 ᴄʜᴀɴɴᴇʟ 1', url="t.me/malayalammovies_2023"),
-            InlineKeyboardButton('📮 ᴄʜᴀɴɴᴇʟ 2', url="t.me/popcon2")
+            InlineKeyboardButton('📮 ᴄʜᴀɴɴᴇʟ 2', url="t.me/xeonflixmovies")
        ],[
             InlineKeyboardButton('📮 ᴄʜᴀɴɴᴇʟ 3', url="https://t.me/CInemavillaz_20"),
             InlineKeyboardButton('📮 ᴄʜᴀɴɴᴇʟ 4', url="https://t.me/+Al6Dl5ZY5aFmN2U1")
@@ -3095,7 +3095,7 @@ async def auto_filter(client, msg, spoll=False):
             ]
         )
         btn.insert(0, [
-            InlineKeyboardButton("ᴘʀᴇᴍɪᴜᴍ", url=f"https://t.me/OtakuFlix_Network/4639"),
+            InlineKeyboardButton("ᴘʀᴇᴍɪᴜᴍ", url=f"https://t.me/Mr_YKYB"),
             InlineKeyboardButton("sᴇɴᴅ ᴀʟʟ", callback_data=f"sendfiles#{key}")
         ])
 
